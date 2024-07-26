587b3583eafc87c3d3d33d081b184f6211ba5d15
lcovarrubias <lcovarrubias@cys.com.ar>
2024-07-26 13:55:37 -0300

    Botones de las tareas: se crean las funciones para editar una tarea y la funcion para contar las tareas completas e incompletas

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+const completedCounter = document.getElementById("completed-counter");
+const uncompletedCounter = document.getElementById("uncompleted-counter");
+
 
 function addTask() {
     const task = inputBox.value.trim();
@@ -21,4 +24,35 @@ function addTask() {
 
     listContainer.appendChild(li);
     inputBox.value = "";
+
+    const checkbox = li.querySelector("input");
+    const editBtn = li.querySelector(".edit-btn");
+    const taskSpan = li.querySelector("span");
+    const deleteBtn = li.querySelector(".delete-btn");
+
+    checkbox.addEventListener("click", function() {
+        li.classList.toggle("completed", checkbox.checked);
+        updateCounters();
+    });
+
+    editBtn.addEventListener("click", function(){
+        const update = prompt("Edit task:", taskSpan.textContent);
+        if (update !== null) {
+            taskSpan.textContent = update;
+            li.classList.remove("completed");
+            checkbox.checked = false;
+            updateCounters();
+        }
+    });
 }
+
+function updateCounters(){
+    const completedTasks = document.querySelectorAll(".completed").length;
+    const uncompletedTasks = document.querySelectorAll("li:not(.completed)").length;
+
+    completedCounter.textContent = completedTasks;
+    uncompletedCounter.textContent = uncompletedTasks;
+
+    updateCounters();
+}
+
